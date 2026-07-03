ddff6a7cdf029849b7b19c24da0ecc667365ec47
Bread <hyh3322@gmail.com>
2026-07-03 16:13:54 +0900

    카카오톡 공유 기능 추가

---
+++ js/kakao.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    Kakao.init("7400414801fa68a6bec478724cb92a3b");
+
+});
+
+function shareKakao(){
+
+    Kakao.Share.sendDefault({
+
+        objectType: "feed",
+
+        content: {
+
+            title: "황영환 ♥ 신부 결혼식",
+
+            description: "2026년 9월 20일 오전 11시\n광명무역센터컨벤션 3F 그랜드볼룸",
+
+            imageUrl: "https://bread2951.github.io/mobile-invitation/images/hero.jpg",
+
+            link: {
+
+                mobileWebUrl: "https://bread2951.github.io/mobile-invitation/",
+
+                webUrl: "https://bread2951.github.io/mobile-invitation/"
+
+            }
+
+        },
+
+        buttons: [
+
+            {
+
+                title: "청첩장 보기",
+
+                link: {
+
+                    mobileWebUrl: "https://bread2951.github.io/mobile-invitation/",
+
+                    webUrl: "https://bread2951.github.io/mobile-invitation/"
+
+                }
+
+            }
+
+        ]
+
+    });
+
+}
