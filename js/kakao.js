8384fe8502967a52b464dd47b04814ad50b8821e
Bread <hyh3322@gmail.com>
2026-07-03 16:36:20 +0900

    카카오 공유 수정

--- js/kakao.js
+++ js/kakao.js
@@ -1,51 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
-
-    Kakao.init("7400414801fa68a6bec478724cb92a3b");
-
+    if (!Kakao.isInitialized()) {
+        Kakao.init("7400414801fa68a6bec478724cb92a3b");
+    }
 });
 
-function shareKakao(){
+function shareKakao() {
+
+    const currentUrl = window.location.origin + window.location.pathname;
 
     Kakao.Share.sendDefault({
-
         objectType: "feed",
 
         content: {
-
-            title: "황영환 ♥ 신부 결혼식",
-
+            title: "황영환 ♥ 이미선 결혼식",
             description: "2026년 9월 20일 오전 11시\n광명무역센터컨벤션 3F 그랜드볼룸",
-
-            imageUrl: "https://bread2951.github.io/mobile-invitation/images/hero.jpg",
-
+            imageUrl: currentUrl.replace(/\/$/, "") + "/images/hero.jpg",
             link: {
-
-                mobileWebUrl: "https://bread2951.github.io/mobile-invitation/",
-
-                webUrl: "https://bread2951.github.io/mobile-invitation/"
-
+                mobileWebUrl: currentUrl,
+                webUrl: currentUrl
             }
-
         },
 
         buttons: [
-
             {
-
                 title: "청첩장 보기",
-
                 link: {
-
-                    mobileWebUrl: "https://bread2951.github.io/mobile-invitation/",
-
-                    webUrl: "https://bread2951.github.io/mobile-invitation/"
-
+                    mobileWebUrl: currentUrl,
+                    webUrl: currentUrl
                 }
-
             }
-
         ]
-
     });
 
 }
